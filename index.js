b55a44de7f7bd376db520dedb7e093ced14e96a2
Brandon Rodriguez <chilebr99@gmail.com>
2023-01-04 21:08:43 -0500

    Got api hex colors to return back

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("test")
+const colorValue = document.getElementById("head");
+const colorScheme = document.getElementById("lang");
+const colorSchemeBtn = document.getElementById("color-btn");
+
+
+
+colorSchemeBtn.addEventListener("click", function(event) {
+    let currentColor = colorValue.value.replace("#","")
+    currentColorScheme = colorScheme.value
+    console.log(currentColorScheme)
+    console.log(currentColor)
+    event.preventDefault()
+    
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentColorScheme}`)
+    .then((response) => response.json())
+    .then((data) => {
+
+        for(i = 0; i < data.colors.length; i++) {
+            const hexColor = data.colors[i].hex.value
+            console.log(hexColor)
+        }
+       
+        // console.log(data.colors[0].hex.value)
+        // console.log(data.colors[1].hex.value)
+        // console.log(data.colors[2].hex.value)
+        // console.log(data.colors[3].hex.value)
+        // console.log(data.colors[4].hex.value)
+       
+        
+       
+
+
+    
+        
+        
+    })
+    
+    
+})
+
