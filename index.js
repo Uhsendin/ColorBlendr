a04fcb335845059076c8dfa01fc056d4b5d10eaf
Brandon Rodriguez <chilebr99@gmail.com>
2023-07-10 12:36:11 -0400

    removed old css & formatted js

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const colorValue = document.getElementById("head");
-const colorScheme = document.getElementById("lang");
-const colorSchemeBtn = document.getElementById("color-btn");
+const colorValue = document.getElementById('head');
+const colorScheme = document.getElementById('lang');
+const colorSchemeBtn = document.getElementById('color-btn');
 
+colorSchemeBtn.addEventListener('click', function(event) {
+    let currentColor = colorValue.value.replace('#', '');
+    currentColorScheme = colorScheme.value;
+    event.preventDefault();
 
-
-colorSchemeBtn.addEventListener("click", function(event) {
-    let currentColor = colorValue.value.replace("#","")
-    currentColorScheme = colorScheme.value
-    event.preventDefault()
-    
-
-    fetch(`https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentColorScheme}`)
-    .then((response) => response.json())
-    .then((data) => {
-
-        for(i = 0; i < data.colors.length; i++) {
-            const hexColor = data.colors[i].hex.value
-            const rectangle = document.querySelectorAll(".rectangle")
-            const hexText = document.querySelectorAll(".para")
-            rectangle[i].style.backgroundColor = hexColor
-            hexText[i].textContent = hexColor
-        }
-       
-       
-       
-        
-       
-
-
-    
-        
-        
-    })
-    
-    
-})
-
+    fetch(
+        `https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentColorScheme}`,
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            for (i = 0; i < data.colors.length; i++) {
+                const hexColor = data.colors[i].hex.value;
+                const rectangle = document.querySelectorAll('.rectangle');
+                const hexText = document.querySelectorAll('.para');
+                rectangle[i].style.backgroundColor = hexColor;
+                hexText[i].textContent = hexColor;
+            }
+        });
+});
